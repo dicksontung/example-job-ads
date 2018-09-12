d5bd964bfa2cef7cc1ad2372368006eaebd1738a
dickson.tung <tungdickson@gmail.com>
2018-09-12 11:19:08 +0800

    Init

---
+++ src/main/kotlin/com/dickson/job/ads/AdsApplication.kt
@@ -0,0 +1,11 @@
+package com.dickson.job.ads
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdsApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdsApplication>(*args)
+}
